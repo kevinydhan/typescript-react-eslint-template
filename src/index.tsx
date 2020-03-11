e8db92c75371780b388f9a169d9c8b5e9c676aec
Kevin Han <kevinhanyd@gmail.com>
2020-03-11 09:53:57 -0400

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function App(): JSX.Element {
+    return <div>Hello world</div>
+}
+
+render(<App />, document.getElementById('app'))
